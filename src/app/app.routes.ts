77ffdd6fb5d5fd833fa7ba983865e2cbf286309f
Arman Rasouli <paplix.dev@gmail.com>
2024-12-31 11:28:44 +0800

    angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,17 @@ import { AdminDashboardComponent } from './components/admin-dashboard/admin-dash
 import { UserDashboardComponent } from './components/user-dashboard/user-dashboard.component';
 import { LoginComponent } from './components/login/login.component';
 import { EditUserComponent } from './components/edit-user/edit-user.component';
+import { TimesheetComponent } from './components/timesheet/timesheet.component';
 import { AuthGuard } from './auth.guard'; // Import the AuthGuard
 import { RoleGuard } from './role.guard'; // Import the RoleGuard
 
 export const routes: Routes = [
+  { 
+    path: 'timesheet', 
+    component: TimesheetComponent, 
+    // canActivate: [AuthGuard, RoleGuard], // Protect with both AuthGuard and RoleGuard
+    // data: { role: 'admin' } // Restrict to 'admin' role
+  },
   { 
     path: 'admin-dashboard', 
     component: AdminDashboardComponent, 
